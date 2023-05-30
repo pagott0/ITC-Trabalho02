aae5b42d36a30afb862c7454da92b7eb4474eb7a
Bruno Volpe <bruno@Brunos-MacBook-Pro.local>
2023-05-30 17:15:11 -0300

    site template

--- src/index.js
+++ src/index.js
@@ -1,2 +1,28 @@
 import './style.css'
-console.log('EIIIIIIIIIIIIIIIIIIIIIII');
+let toggle = false;
+const toggleBox = (text, eventName, eventYear) => {
+    const html = `
+    <div class="timeline-item ">
+            <div class="content ${toggle ? "right" : ""}">
+                <h2>${eventName}</h2>
+                <p>${text}</p>
+            </div>
+            <div class="timeline-icon">${eventYear}</div>
+        </div>
+    `
+
+
+    const timeline = document.querySelector(".timeline");
+    const element = document.createElement("div");
+    element.innerHTML = html;
+    timeline.appendChild(element);
+    toggle = !toggle;
+}
+
+/* Call this function to add events, first parameter: text, second parameter: event name, third parameted: year */
+toggleBox("Lorem ipsum dolor sit amet, consectetur adipiscing elit.", "Event 1", 1);
+toggleBox("Lorem ipsum dolor sit amet, consectetur adipiscing elit.", "Event 2", 2);
+toggleBox("Lorem ipsum dolor sit amet, consectetur adipiscing elit.", "Event 3", 3);
+
+
+console.log(111232132)
