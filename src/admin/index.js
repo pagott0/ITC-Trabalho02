fe81a8bf004941ae700e6d77aacbd40d2eaaeee4
Bruno Volpe <bruno@Brunos-MacBook-Pro.local>
2023-06-06 13:48:12 -0300

    desactivate authentication for development

--- src/admin/index.js
+++ src/admin/index.js
@@ -15,7 +15,7 @@ onAuthStateChanged(auth, (user) => {
             localStorage.setItem("user", JSON.stringify(user));
         } else {
             window.alert("You are not authorized to access this page.");
-            loadPage()
+            loadPage() //TODO: REMOVE THIS
         }
     } else {
         // Usuário não está logado, exibe o popup de login
